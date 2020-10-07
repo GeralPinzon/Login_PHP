366a789588a83d980bd53f3f136d9103048f5cae
geraldin <geraldinpinzonb@gmail.com>
2020-10-06 22:44:55 -0500

    prueba

---
+++ new_pass.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Esttodo esto esta en recoverpassword -->
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Login</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' href='css/styles.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+<div class="login">
+    <img class="logo" src="Imagenes/logo efecto.png" alt="Fondo">
+    <h1>New password</h1>
+    <form method="POST">
+        <!--Username-->
+        <label for="email">New password</label>
+        <input type="password" name="new_pass_c" id="new_pass_c" placeholder="Enter your new password">
+        <input type="submit" name="new_password" value="Submit">
+    </form>
+    <?php include ("");?>
+</div>
+</body>
+</html>
